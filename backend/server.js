b38797bd8eab4f3e6f0c7b5a0e0b038a932b1f5b
Pedro Ribas <pedroibribas@gmail.com>
2022-08-15 09:11:13 -0400

    Set production mode

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,18 @@ app.use('/api/clients', require('./routes/clientRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/pdf', require('./routes/pdfRoute'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+};
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
